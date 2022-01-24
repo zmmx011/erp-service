0c5fd9e42b39d2281eac9f73de5f627153882046
damu <damu@inveniacorp.com>
2022-01-24 23:18:47 +0900

    IT 팀 추가

--- src/main/java/com/invenia/erpservice/kafka/vacation/VacationConsumer.java
+++ src/main/java/com/invenia/erpservice/kafka/vacation/VacationConsumer.java
@@ -12,7 +12,6 @@ import com.invenia.erpservice.user.UserNotFoundException;
 import com.invenia.erpservice.user.UserService;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -39,50 +38,54 @@ public class VacationConsumer {
       log.error(e.getMessage(), e);
     }
 
-    List<Integer> targetDept = List.of(2039); // IT 개발파트
     VacationPayload payload = topic.getPayload();
     UserEntity user = userService.getUserByEmpSeq(payload.getEmpSeq()).orElseThrow(UserNotFoundException::new);
 
-    if (targetDept.contains(user.getDeptSeq())) {
-      String text = "";
-      text += payload.getPrevUseDays().equals("AMNQ") ? "**[반차]** " : "";
-      if (payload.getWkFrDate().equals(payload.getWkToDate())) {
-        text += LocalDate.parse(payload.getWkFrDate(), DateTimeFormatter.BASIC_ISO_DATE);
-      } else {
-        text += LocalDate.parse(payload.getWkFrDate(), DateTimeFormatter.BASIC_ISO_DATE)
-            + " ~ "
-            + LocalDate.parse(payload.getWkToDate(), DateTimeFormatter.BASIC_ISO_DATE);
-      }
-
-      Message message = Message.builder()
-          .channelId("wjowika97tbqxd31sgjmddqier")
-          .message("")
-          .props(Props.builder()
-              .attachment(AttachmentsItem.builder()
-                  .title(user.getUserName() + "님의 새로운 휴가 일정이 등록되었습니다.")
-                  .text(text)
-                  .build())
-              .build())
-          .build();
-
-      WebClient webClient = WebClient.builder()
-          .baseUrl("https://slack.inveniacorp.com")
-          .clientConnector(new ReactorClientHttpConnector(
-              HttpClient.create().wiretap(true)
-          ))
-          .build();
-
-      String result = webClient
-          .post()
-          .uri("/api/v4/posts")
-          .contentType(MediaType.APPLICATION_JSON)
-          .headers(header -> header.setBearerAuth("99uqgienebf5td4ztntemti4me"))
-          .bodyValue(message)
-          .retrieve()
-          .bodyToMono(String.class)
-          .block();
-
-      log.info("Mattermost API Response ==> {}", result);
+    if (user.getDeptSeq() == 2039) { // IT 개발파트
+      callMattermostAPI(payload, user.getUserName(), "wjowika97tbqxd31sgjmddqier");
+    } else if (user.getDeptSeq() == 1681) { // IT 팀
+      callMattermostAPI(payload, user.getUserName(), "35nctfyuzbyuj8hpu5qwnqmroh");
     }
+  }
+
+  private void callMattermostAPI(VacationPayload payload, String userName, String channelId) {
+    String text = "";
+    text += payload.getPrevUseDays().equals("AMNQ") ? "**[반차]** " : "";
+    if (payload.getWkFrDate().equals(payload.getWkToDate())) {
+      text += LocalDate.parse(payload.getWkFrDate(), DateTimeFormatter.BASIC_ISO_DATE);
+    } else {
+      text += LocalDate.parse(payload.getWkFrDate(), DateTimeFormatter.BASIC_ISO_DATE)
+          + " ~ "
+          + LocalDate.parse(payload.getWkToDate(), DateTimeFormatter.BASIC_ISO_DATE);
+    }
+
+    Message message = Message.builder()
+        .channelId(channelId)
+        .props(Props.builder()
+            .attachment(AttachmentsItem.builder()
+                .title(userName + "님의 새로운 휴가 일정이 등록되었습니다.")
+                .text(text)
+                .build())
+            .build())
+        .build();
+
+    WebClient webClient = WebClient.builder()
+        .baseUrl("https://slack.inveniacorp.com")
+        .clientConnector(new ReactorClientHttpConnector(
+            HttpClient.create().wiretap(true)
+        ))
+        .build();
+
+    String result = webClient
+        .post()
+        .uri("/api/v4/posts")
+        .contentType(MediaType.APPLICATION_JSON)
+        .headers(header -> header.setBearerAuth("99uqgienebf5td4ztntemti4me"))
+        .bodyValue(message)
+        .retrieve()
+        .bodyToMono(String.class)
+        .block();
+
+    log.info("Mattermost API Response ==> {}", result);
   }
 }
