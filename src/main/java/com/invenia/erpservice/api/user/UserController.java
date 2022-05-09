b272a8e5d278f70046d1c96f7afc7cb69fe90936
damu <damu@inveniacorp.com>
2022-05-09 13:50:11 +0900

    add swagger

--- src/main/java/com/invenia/erpservice/api/user/UserController.java
+++ src/main/java/com/invenia/erpservice/api/user/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("erp-service/v1/users")
+@RequestMapping("/erp-service/v1/users")
 public class UserController {
 
   private final UserService userService;
